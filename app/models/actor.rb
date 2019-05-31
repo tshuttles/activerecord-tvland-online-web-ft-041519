375646d6834c5aa0b570556dce41bf43852c58b4
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-31 04:19:26 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,9 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles 
-    self.characters
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end 
   
 end
