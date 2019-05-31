466e56e50be65e64c50ac112a7bc55deb2953170
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-31 04:45:48 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_day_and_season_to_shows.rb
+++ db/migrate/008_add_day_and_season_to_shows.rb
@@ -1,7 +1,8 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   
   def change
-    add_column :characters, :catchphrase, :string 
+    add_column :name, :day, :string 
+    add_column :name, :season, :string 
   end
   
 end
